5f7f0209bb89d12db032fe952bf27553a8327ea4
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2024-05-01 14:35:08 -0300

    Init commit

--- index.js
+++ index.js
@@ -23,32 +23,3 @@ glob('**/node_modules').then((folders) => {
         });
       });
 })
-
-
-
-// // Buscar todos os diretórios node_modules a partir do diretório atual
-// glob('**/node_modules', { cwd: process.cwd() }, (err, matches) => {
-//     console.log("Started.")
-//   if (err) {
-//     console.error('Erro ao buscar node_modules:', err);
-//     process.exit(1);
-//   }
-
-//   if (matches.length === 0) {
-//     console.log('Nenhuma pasta node_modules encontrada.');
-//     return;
-//   }
-
-//   console.log(`Encontradas ${matches.length} pastas node_modules. Excluindo...`);
-
-//   // Excluir cada diretório encontrado
-//   matches.forEach((dir) => {
-//     rimraf(path.join(process.cwd(), dir), (rimrafErr) => {
-//       if (rimrafErr) {
-//         console.error(`Erro ao excluir ${dir}:`, rimrafErr);
-//       } else {
-//         console.log(`Excluído: ${dir}`);
-//       }
-//     });
-//   });
-// });
